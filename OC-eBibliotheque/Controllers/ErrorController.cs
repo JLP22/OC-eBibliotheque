aa2efaa2a28f4b58a3bb005c9a8829f596205f5d
lepouliquen <lepouliquen@gmail.com>
2018-12-13 22:38:42 +0100

    Ajout gestion erreur

---
+++ OC-eBibliotheque/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OC_eBibliotheque.Controllers
+{
+    public class ErrorController : Controller
+    {
+        /* 
+        // GET: /Error/  
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+        public ActionResult NotFound()
+        {
+            return View("Error");
+        }*/
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = (int)System.Net.HttpStatusCode.NotFound;
+            Response.StatusDescription = "404 Not Found";
+
+            return View("Error");
+        }
+    }
+}
---
+++ OC-eBibliotheque/Controllers/NotFoundControllerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OC_eBibliotheque.Controllers
+{
+    public class NotFoundControllerFactory : DefaultControllerFactory
+    {
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            // If controller not found it will be null, so we want to take control
+            // of the request and send it to the ErrorController.NotFound method.
+            if (controllerType == null)
+            {
+                requestContext.RouteData.Values["action"] = "NotFound";
+                requestContext.RouteData.Values["controller"] = "Error";
+                return base.GetControllerInstance(requestContext, typeof(ErrorController));
+            }
+
+            return base.GetControllerInstance(requestContext, controllerType);
+        }
+    }
+}
